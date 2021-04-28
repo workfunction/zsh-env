980bf33dda255c3fb99bf0c66a3d60fc2032fffc
Jason Huang <jason840507@gmail.com>
2021-04-28 09:38:04 +0800

    [Feature] Restore previous user config

--- install.sh
+++ install.sh
@@ -1,39 +1,51 @@
-current_path=$(realpath .)
+update_submodules() {
+    echo "Updating submodules..."
+    git submodule foreach 'git checkout master || git checkout main' > /dev/null 2>&1
+    git submodule foreach git pull origin --ff-only > /dev/null 2>&1
+}
 
-# update submodules
-echo "Updating submodules..."
-git submodule foreach git pull --ff-only > /dev/null
+install_submodules() {
+    # install autojump
+    echo "Installing autojump"
+    mkdir -p autojump
+    autojump_path=$PWD/autojump
 
-# backup old zshrc
-mkdir -p backups
-backup_path=$current_path/backups
-latest=$backup_path/$(ls -t1 $backup_path |  head -n 1)
-backup=$backup_path/zshrc.$(mktemp -u | grep -oP "\.\K.*")
+    cd libs/autojump
+    ./install.py -d $autojump_path > /dev/null
+    cd -
 
-[ ! -f $latest ] && touch $backup && latest=$backup
+    # install fzf
+    echo "Installing fzf"
+    libs/fzf/install --all --no-update-rc > /dev/null
+}
 
-[ -f $HOME/.zshrc ] && \
-! grep -q "###### auto generated .zshrc" $HOME/.zshrc && \
-! diff $HOME/.zshrc $latest > /dev/null && \
-cp $HOME/.zshrc $backup && \
-echo "Old .zshrc backup to $backup..."
+backup_zshrc() {
+    mkdir -p backups
+    backup_path=$PWD/backups
+    latest=$backup_path/$(ls -t1 $backup_path |  head -n 1)
+    backup=$backup_path/zshrc.$(mktemp -u | grep -oP "\.\K.*")
 
-# create new zshrc
-echo "Creating new zshrc file"
-sed "s|###PWD###|$current_path|g" scripts/configs.zsh-template > $HOME/.zshrc
-cat scripts/zshrc.zsh-template >> $HOME/.zshrc
+    # check if no previous backups
+    [ ! -f $latest ] && touch $backup && latest=$backup
 
-# install autojump
-echo "Installing autojump"
-mkdir -p autojump
-autojump_path=$current_path/autojump
+    [ -f $HOME/.zshrc ] && \
+    ! diff $HOME/.zshrc $latest > /dev/null && \
+    cp $HOME/.zshrc $backup && \
+    echo "Old .zshrc backup to $backup..."
+}
 
-cd libs/autojump
-./install.py -d $autojump_path > /dev/null
-cd -
+install_zshrc() {
+    echo "Creating new zshrc file"
+    sed "s|###PWD###|$PWD|g" scripts/configs.zsh-template > $HOME/.zsh_configs
 
-# install fzf
-echo "Installing fzf"
-libs/fzf/install --all > /dev/null
+    user_conf="$(sed -e '1,/# Put your custom scripts below/d' $HOME/.zshrc)"
+    sed "s|###PWD###|$PWD|g" scripts/zshrc.zsh-template > $HOME/.zshrc
+    echo "$user_conf" >> $HOME/.zshrc
+}
+
+update_submodules;
+install_submodules;
+backup_zshrc;
+install_zshrc;
 
 echo "Done!"
